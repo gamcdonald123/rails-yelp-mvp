4e6611390341a6a7c7d5bafb5dcebd1cb1eb2ff4
Guy McDonald <guy@goodfishgroup.com>
2024-02-21 17:59:50 +0000

    Final end of day

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,15 @@
 class ReviewsController < ApplicationController
   def new
-    ...
+    @plant = Plant.new
+  end
+
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new(review)
+
+    if
+
+    else
+    end
   end
 end
